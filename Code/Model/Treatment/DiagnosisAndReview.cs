670b483ed2cae379609bde4ff90a3c7fbdaaad18
Nemanja Dimitrijevic <necapro@gmail.com>
2020-06-09 17:12:45 +0200

    Napravljen repozitorijum za DiagnosisAndReview i dodata Main klasa za testiranje

--- Code/Model/Treatment/DiagnosisAndReview.cs
+++ Code/Model/Treatment/DiagnosisAndReview.cs
@@ -10,8 +10,26 @@ namespace Model.Treatment
 {
    public class DiagnosisAndReview
    {
-      private String Diagnosis;
-      private String Review;
-   
-   }
+        private int id;
+      private String diagnosis;
+      private String review;
+
+        public string Diagnosis { get => diagnosis; set => diagnosis = value; }
+        public string Review { get => review; set => review = value; }
+        public int Id { get => id; set => id = value; }
+
+        public DiagnosisAndReview(string diagnosis, string review, int id)
+        {
+            Diagnosis = diagnosis;
+            Review = review;
+            Id = id;
+        }
+
+        public DiagnosisAndReview()
+        {
+            Id = 0;
+            Diagnosis = "Neka dijagnoza";
+            Review = "Neka procedura";
+        }
+    }
 }
