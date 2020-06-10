d009f1f1589b42b8928a53ae5b8df95115b72d20
Nemanja Dimitrijevic <necapro@gmail.com>
2020-06-10 18:52:22 +0200

    Dodat repository za djagnozu i recept

--- Code/Model/Treatment/Treatment.cs
+++ Code/Model/Treatment/Treatment.cs
@@ -4,23 +4,32 @@
  * Purpose: Definition of the Class Treatment.Treatment
  ***********************************************************************/
 
+using Model.SystemUsers;
 using System;
 
 namespace Model.Treatment
 {
    public class Treatment
-   {  
-      /// <pdGenerated>default getter</pdGenerated>
-     
-      public Prescription prescription;
-      public ScheduledSurgery scheduledSurgery;
-      public DiagnosisAndReview diagnosisAndReview;
-      public ReferralToHospitalTreatment referralToHospitalTreatment;
-      public Model.SystemUsers.Doctor doctor;
-   
-      private DateTime FromDate;
-      private DateTime EndDate;
-      private int Id;
-   
-   }
+   {
+        /// <pdGenerated>default getter</pdGenerated>
+
+        private Prescription prescription;
+        private ScheduledSurgery scheduledSurgery;
+        private DiagnosisAndReview diagnosisAndReview;
+        private ReferralToHospitalTreatment referralToHospitalTreatment;
+        private Model.SystemUsers.Doctor doctor;
+
+        private DateTime fromDate;
+      private DateTime endDate;
+      private long id;
+
+        public Prescription Prescription { get => prescription; set => prescription = value; }
+        public ScheduledSurgery ScheduledSurgery { get => scheduledSurgery; set => scheduledSurgery = value; }
+        public DiagnosisAndReview DiagnosisAndReview { get => diagnosisAndReview; set => diagnosisAndReview = value; }
+        public ReferralToHospitalTreatment ReferralToHospitalTreatment { get => referralToHospitalTreatment; set => referralToHospitalTreatment = value; }
+        public DateTime FromDate { get => fromDate; set => fromDate = value; }
+        public DateTime EndDate { get => endDate; set => endDate = value; }
+        public long Id { get => id; set => id = value; }
+        public Doctor Doctor { get => doctor; set => doctor = value; }
+    }
 }
--- Code/Repository/Csv/Converter/TreatmentCSVConverter.cs
+++ Code/Repository/Csv/Converter/TreatmentCSVConverter.cs
@@ -4,6 +4,7 @@
  * Purpose: Definition of the Class Repository.Csv.Converter.TreatmentCSVConverter
  ***********************************************************************/
 
+using Model.Rooms;
 using Model.Treatment;
 using System;
 
@@ -15,12 +16,32 @@ namespace Repository.Csv.Converter
 
         public Treatment ConvertCSVFormatToEntity(string entityCSVFormat)
         {
-            throw new NotImplementedException();
+            string[] tokens = entityCSVFormat.Split(Delimiter.ToCharArray());
+            long id = long.Parse(tokens[0]);
+//            Doctor doctor = new Doctor();
+            DateTime fromDate = DateTime.Parse(tokens[2]);
+            DateTime toDate = DateTime.Parse(tokens[3]);
+
+
+            return null;
         }
 
         public string ConvertEntityToCSVFormat(Treatment entity)
         {
-            throw new NotImplementedException();
+            string doctor = entity.Doctor.ToString();
+            string fromDate = entity.FromDate.ToString("dd-MM-yyyy");
+            string toDate = entity.EndDate.ToString("dd-MM-yyyy");
+            string id = entity.Id.ToString();
+            string prescription = "";
+            foreach (Drug drug in entity.Prescription.Drug)
+            {
+                prescription += drug.Name + ",";
+            }
+            string scheduledSurgery = entity.ScheduledSurgery.StartDate + "," + entity.ScheduledSurgery.EndDate + "," + entity.ScheduledSurgery.CauseForOperation + "," + entity.ScheduledSurgery.Surgeon;
+            string diagnosisAndReview = entity.DiagnosisAndReview.Diagnosis + entity.DiagnosisAndReview.Review;
+            string referralToAHospitalTreatment = entity.ReferralToHospitalTreatment.StartDate.ToString() + entity.ReferralToHospitalTreatment.EndDate.ToString() + entity.ReferralToHospitalTreatment.CauseForHospTreatment;
+
+            return string.Join(Delimiter, id, doctor, fromDate, toDate, prescription, scheduledSurgery, diagnosisAndReview, referralToAHospitalTreatment);
         }
     }
 }
