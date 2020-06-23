db13aa31874c52b7ba0933f64b519dc984e6e7db
Aleksa1998 <akimbo.santrac@gmail.com>
2020-06-23 23:18:23 +0200

    Drug repository

--- Code/Repository/DrugRepository.cs
+++ Code/Repository/DrugRepository.cs
@@ -4,39 +4,71 @@
  * Purpose: Definition of the Class Repository.UserRepository
  ***********************************************************************/
 
+using health_clinicClassDiagram.Repository.Sequencer;
 using Model.Rooms;
+using Repository.Csv.Stream;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Repository
 {
    public class DrugRepository : IDrugRepository
    {
-      public DrugRepository GetInstance() { return null; }
+        private readonly ICSVStream<Drug> _stream;
+        private readonly iSequencer<long> _sequencer;
 
-        public Drug GetDrug(Drug drug)
+        private String _path;
+        private static DrugRepository Instance;
+
+        public DrugRepository GetInstance() { return null; }
+
+        public DrugRepository(string path, CSVStream<Drug> stream, iSequencer<long> sequencer)
         {
-            throw new NotImplementedException();
+            _path = path;
+            _stream = stream;
+            _sequencer = sequencer;
+            _sequencer.Initialize(GetMaxId(_stream.ReadAll()));
+        }
+        private long GetMaxId(List<Drug> equip)
+        {
+            return equip.Count() == 0 ? 0 : equip.Max(eq => eq.Id);
         }
 
         public Drug Save(Drug obj)
         {
-            throw new NotImplementedException();
+            _stream.AppendToFile(obj);
+            return obj;
         }
 
         public Drug Edit(Drug obj)
         {
-            throw new NotImplementedException();
+            var drugs = _stream.ReadAll().ToList();
+            drugs[drugs.FindIndex(dr => dr.Id == obj.Id)] = obj;
+            _stream.SaveAll(drugs);
+            return obj;
         }
 
         public bool Delete(Drug obj)
         {
-            throw new NotImplementedException();
+            var drugs = _stream.ReadAll().ToList();
+            var drugsToRemove = drugs.SingleOrDefault(dr => dr.Id == obj.Id);
+            if (drugsToRemove != null)
+            {
+                drugs.Remove(drugsToRemove);
+                _stream.SaveAll(drugs);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public List<Drug> GetAll()
         {
-            throw new NotImplementedException();
+            var eq = (List<Drug>)_stream.ReadAll();
+            return eq;
         }
 
         public bool OpenFile(string path)
@@ -49,8 +81,27 @@ namespace Repository
             throw new NotImplementedException();
         }
 
-        private String Path;
-      private static DrugRepository Instance;
-   
-   }
+        public bool DrugExists(string naziv)
+        {
+            var drugs = _stream.ReadAll().ToList();
+            foreach (Drug d in drugs)
+            {
+                if (d.Name.Equals(naziv))
+                {
+                    return true;
+                    break;
+                }
+            }
+            return false;
+        }
+
+        public Drug GetDrug(string naziv)
+        {
+            var drug = _stream.ReadAll().ToList();
+            return drug[drug.FindIndex(apt => apt.Name.Equals(naziv))];
+        }
+
+
+
+    }
 }
