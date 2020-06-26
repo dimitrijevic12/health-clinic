1f1a5cb47653da17a872021596b8a36f59c1d6df
Stefann11 <santrac.stefan@gmail.com>
2020-06-26 05:52:17 -1000

    Ispravljanje CSV convertera

--- Code/Repository/Csv/Converter/AppointmentCSVConverter.cs
+++ Code/Repository/Csv/Converter/AppointmentCSVConverter.cs
@@ -26,28 +26,11 @@ namespace Repository.Csv.Converter
         public Appointment ConvertCSVFormatToEntity(string entityCSVFormat)
         {
             string[] tokens = entityCSVFormat.Split(_delimiter.ToCharArray());
-            string doctorName = tokens[2];
-            string doctorSurname = tokens[3];
-            string patientName = tokens[4];
-            string patientSurname = tokens[5];
-            long patientId = long.Parse(tokens[6]);
-            ExamOperationRoom room = new ExamOperationRoom(long.Parse(tokens[10]));
-            TypeOfAppointment type = (TypeOfAppointment)Enum.Parse(typeof(TypeOfAppointment), tokens[7], true);
-            DateTime startDate = DateTime.Parse(tokens[8]);
-            DateTime endDate = DateTime.Parse(tokens[9]);
-            //DateTime startDate = DateTime.Now;
-            //DateTime endDate = DateTime.Now;
-
-            //Patient patient = new Patient(tokens[0], tokens[1], int.Parse(tokens[2]));    //preskocimo prvi button select patient
-
-            /*var doctorRepository = new DoctorRepository(
-                "../../Resources/Data/doctors.csv",
-                new CSVStream<Doctor>("../../Resources/Data/doctors.csv", new DoctorCSVConverter(",", "dd.MM.yyyy.")),
-                new LongSequencer());
-            var patientRepository = new PatientRepository(
-                "../../Resources/Data/patients.csv",
-                new CSVStream<Patient>("../../Resources/Data/patients.csv", new PatientCSVConverter(",", "dd.MM.yyyy.")),
-                new LongSequencer());*/
+            long patientId = long.Parse(tokens[2]);
+            ExamOperationRoom room = new ExamOperationRoom(long.Parse(tokens[6]));
+            TypeOfAppointment type = (TypeOfAppointment)Enum.Parse(typeof(TypeOfAppointment), tokens[3], true);
+            DateTime startDate = DateTime.Parse(tokens[4]);
+            DateTime endDate = DateTime.Parse(tokens[5]);
 
             var doctorRepository = DoctorRepository.Instance;
             var patientRepository = PatientRepository.Instance;
@@ -63,17 +46,13 @@ namespace Repository.Csv.Converter
                 room,
                 type,
                 startDate,
-                endDate); //tokens[6]
+                endDate);
         }
 
         public string ConvertEntityToCSVFormat(Appointment entity)
              => string.Join(_delimiter,
                entity.Id,
                entity.Doctor.Id,
-               entity.Doctor.Name,
-               entity.Doctor.Surname,
-               entity.Patient.Name,
-               entity.Patient.Surname,
                entity.Patient.Id,
                entity.Type,
                entity.StartDate.ToString(),
--- Code/Repository/Csv/Converter/MedicalRecordCSVConverter.cs
+++ Code/Repository/Csv/Converter/MedicalRecordCSVConverter.cs
@@ -31,67 +31,18 @@ namespace Repository.Csv.Converter
         {
             string[] tokens = entityCSVFormat.Split(_delimiter.ToCharArray());
 
-            String genderString = tokens[5];
-
-            Gender gender = (Gender)Enum.Parse(typeof(Gender), genderString, true);
-
-            DateTime dateOfBirth = DateTime.Parse(tokens[4]);
-
-            //Patient patient = new Patient(tokens[1], tokens[2], long.Parse(tokens[3]), dateOfBirth, gender);
-
-            long idDoctor = long.Parse(tokens[6]);
-
-            /*var doctorRepository = new DoctorRepository(
-                "../../Resources/Data/doctors.csv",
-                new CSVStream<Doctor>("../../Resources/Data/doctors.csv", new DoctorCSVConverter(",", "dd.MM.yyyy.")),
-                new LongSequencer());
-            var patientRepository = new PatientRepository(
-                "../../Resources/Data/patients.csv",
-                new CSVStream<Patient>("../../Resources/Data/patients.csv", new PatientCSVConverter(",", "dd.MM.yyyy.")),
-                new LongSequencer()); */
-
             var doctorRepository = DoctorRepository.Instance;
             var patientRepository = PatientRepository.Instance;
 
-            //Doctor doctor = new Doctor(idDoctor, tokens[7], tokens[8]);
-            Doctor doctor = doctorRepository.GetDoctorById(idDoctor);
-            Patient patient = patientRepository.getPatientById(long.Parse(tokens[3]));
+            Doctor doctor = doctorRepository.GetDoctorById(long.Parse(tokens[2]));
+            Patient patient = patientRepository.getPatientById(long.Parse(tokens[1]));
             List<Treatment> treatments = new List<Treatment>();
 
-            int i = 9;
-            /*int flag = 0;
-            if (tokens.Length >= 10)
+            if (tokens[3] != "")
             {
-                if (tokens[9] != "")
-                {
-                    long id = long.Parse(tokens[9]);
-                    treatments.Add(TreatmentRepository.Instance.GetTreatment(id));
-                }
-            }
-            if (tokens.Length >= 11)
-            {
-                if (tokens[10] != "")
-                {
-                    long id = long.Parse(tokens[10]);
-                    treatments.Add(TreatmentRepository.Instance.GetTreatment(id));
-                }
-            }*/
-
-
-            /*while (i <= tokens.Length-1)
-            {
-                long id = long.Parse(tokens[i]);
-                treatments.Add(TreatmentRepository.Instance.GetTreatment(id));
-                i++;
-            }*/
-
-            if (tokens[9] != "")
-            {
-                String treatmentsString = tokens[9];
+                String treatmentsString = tokens[3];
 
                 String[] oneTreatment = treatmentsString.Split('|');
-
-                Console.WriteLine(oneTreatment.Length);
 
                 for (int j = 0; j < oneTreatment.Length; j++)
                 {
@@ -105,50 +56,34 @@ namespace Repository.Csv.Converter
                 long.Parse(tokens[0]),
                 patient,
                 doctor,
-                treatments); //ne treba new doctor treba promeniti (tokens[6]), kao i u patient u Gender.MALE
+                treatments);
         }
 
         public string ConvertEntityToCSVFormat(MedicalRecord entity)
         {
-            /*String treatments = "";
-            if (entity.Treatments != null)
-            {
-                foreach (Treatment treatment in entity.Treatments)
-                {
-                    if (treatment != null)
-                    {
-                        treatments += treatment.Id + ",";
-                    }
-                }
-            }*/
-
 
             String resenje = "";
 
-
-            Treatment last = entity.Treatments.Last();
-            foreach (Treatment treatment in entity.Treatments)
+            if (entity.Treatments.Count != 0)
             {
-                if (treatment != last)
+                Treatment last = entity.Treatments.Last();
+                foreach (Treatment treatment in entity.Treatments)
                 {
-                    resenje += treatment.Id + "|";
-                }
-                else
-                {
-                    resenje += treatment.Id;
+                    if (treatment != last)
+                    {
+                        resenje += treatment.Id + "|";
+                    }
+                    else
+                    {
+                        resenje += treatment.Id;
+                    }
                 }
             }
 
             return string.Join(_delimiter,
               entity.id,
-              entity.Patient.Name,
-              entity.Patient.Surname,
               entity.Patient.Id,
-              entity.DateOfBirth,
-              entity.Patient.Gender,
               entity.choosenDoctor.Id,
-              entity.choosenDoctor.NameDoctor,
-              entity.choosenDoctor.SurnameDoctor,
               resenje);
 
         }
